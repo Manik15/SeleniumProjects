a97dac54ab4b43d172f879aab0e7e2f58b6e7b2b
Manik15 <32017589+Manik15@users.noreply.github.com>
2017-09-17 00:34:58 +0530

    excel update

--- src/main/java/com/eCom/base/ExcelDataReader.java
+++ src/main/java/com/eCom/base/ExcelDataReader.java
@@ -4,13 +4,11 @@ import java.io.FileInputStream;
 
 public class ExcelDataReader {
 
-	String path;
-	FileInputStream fis;
-	//XSSFWorkbook workBook;
+	
 	
 	public ExcelDataReader()
 	{
-		this.path=path;
+		
 	}
 	
 	public void getExcelData(String sheetName, String rouNum, String colNum)
