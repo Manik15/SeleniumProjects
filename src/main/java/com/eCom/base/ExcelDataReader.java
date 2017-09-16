5a2d5e8e7bd5dedd7600be5a2571cff43032df58
Manik15 <32017589+Manik15@users.noreply.github.com>
2017-09-17 00:24:11 +0530

    excel

---
+++ src/main/java/com/eCom/base/ExcelDataReader.java
@@ -0,0 +1,29 @@
+package com.eCom.base;
+
+import java.io.FileInputStream;
+
+public class ExcelDataReader {
+
+	String path;
+	FileInputStream fis;
+	//XSSFWorkbook workBook;
+	
+	public ExcelDataReader()
+	{
+		this.path=path;
+	}
+	
+	public void getExcelData(String sheetName, String rouNum, String colNum)
+	{
+		try
+		{
+			
+		}
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
