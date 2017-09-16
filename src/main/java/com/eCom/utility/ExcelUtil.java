f49786bdad6e5d460a710b1e994c373c3cab09e3
Rijin Varghese <rvarghes@adobe.com>
2017-09-17 00:08:02 +0530

    added emailUtil

---
+++ src/main/java/com/eCom/utility/ExcelUtil.java
@@ -0,0 +1,17 @@
+package com.eCom.utility;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelUtil {
+public static void main(String[] args) throws IOException {
+	String excelPath="pathToexcel";
+	FileInputStream fis=new FileInputStream(new File(excelPath));
+	XSSFWorkbook workbook=new XSSFWorkbook(fis);
+	
+}
+}
