3fe62e5511701e158a7257a628a57a0411582a5b
Manikantan <Manikantan@DESKTOP-G1BT3BH>
2017-09-17 00:13:20 +0530

    change

--- src/main/java/com/eCom/pageObjects/amazonHomePage.java
+++ src/main/java/com/eCom/pageObjects/amazonHomePage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.DataProvider;
 
-import com.ecommerce.utility.CommonUtitity;
+import com.eCom.utility.CommonUtitity;
 
 public class amazonHomePage {
 
