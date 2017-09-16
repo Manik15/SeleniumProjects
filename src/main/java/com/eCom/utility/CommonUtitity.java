9984393a426f99d88fdd28f1c09bc495b868c84a
Rijin Varghese <rvarghes@adobe.com>
2017-09-17 01:31:06 +0530

    updated commonUtitity

--- src/main/java/com/eCom/utility/CommonUtitity.java
+++ src/main/java/com/eCom/utility/CommonUtitity.java
@@ -6,12 +6,13 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class CommonUtitity {
+	private static final int minWait=10;
 	public static void waitForElementToBeVisible(WebDriver driver, WebElement we) {
-		WebDriverWait wd = new WebDriverWait(driver, 10);
+		WebDriverWait wd = new WebDriverWait(driver, minWait);
 		wd.until(ExpectedConditions.visibilityOf(we));
 	}
 	public static void waitForElementToBeClickable(WebDriver driver, WebElement we) {
-		WebDriverWait wd = new WebDriverWait(driver, 10);
+		WebDriverWait wd = new WebDriverWait(driver, minWait);
 		wd.until(ExpectedConditions.visibilityOf(we));
 	}
 }
